51b62f133ff9e6f4a5e31d4ad70ed394199cb4ae
Александр Прокашев <proxelb@gmail.com>
2022-11-12 15:06:41 +0400

    добавлен класс Authorization для поиска с методом для поиска юзеров по логину и паролю. Поля юзера и персона получили свойство быть равными null

--- Solution/BackendProj/Models/User.cs
+++ Solution/BackendProj/Models/User.cs
@@ -6,17 +6,17 @@ namespace BackendProj.Models
     public class User
     {
         public int Id { get; set; }
-        public string login { get; set; }
-        public string password { get; set; }
+        public string? login { get; set; }
+        public string? password { get; set; }
         [Column(TypeName = "jsonb")]
-        public Person Data { get; set; }
+        public Person? Data { get; set; }
     }
 
     public class Person
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Surname { get; set; }
-        public byte[] Image { get; set; }
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public string? Surname { get; set; }
+        public byte[]? Image { get; set; }
     }
 }
