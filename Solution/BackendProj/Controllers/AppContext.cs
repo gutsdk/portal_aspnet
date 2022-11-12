d44af8a55822a645ecd5e4f1a7f9c076d06bbe74
Siluet3656 <55587056+Siluet3656@users.noreply.github.com>
2022-11-12 15:54:40 +0400

    appContext changes. Fields buty

--- Solution/BackendProj/Controllers/AppContext.cs
+++ Solution/BackendProj/Controllers/AppContext.cs
@@ -4,17 +4,22 @@ using System.Text.Json;
 
 namespace BackendProj.Controllers
 {
-    public class AppContext : DbContext
-    {
-        public DbSet<User> Users { get; set; }
+	public class AppContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		private string Host = "localhost";
+		private string Port = "5433";
+		private string DBname = "usersdb";
+		private string Username = "postgres";
+		private string Password = "password";
 
-        public AppContext()
-        {
-            Database.EnsureCreated();
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=usersdb;Username=postgres;Password=password");
-        }
-    }
+		public AppContext()
+		{
+			Database.EnsureCreated();
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql("Host=" + Host + ";Port=" + Port + ";Database=" + DBname + ";Username=" + Username + ";Password=" + Password);
+		}
+	}
 }
