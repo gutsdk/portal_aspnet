b6aaaf39539c408c42846ad2b3d95d9c65c36b3b
gutsdk <e.zhuykov.02@bk.ru>
2022-11-12 15:14:28 +0400

    добавлена база для фронта

---
+++ Solution/BackendProj/App/containers/header/header.jsx
@@ -0,0 +1,21 @@
+﻿import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <menu>
+                    <ui>
+                        <li>
+                            <Link to="/">Blog</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About me</Link>
+                        </li>
+                    </ui>
+                </menu>
+            </header>
+        )
+    }
+}
