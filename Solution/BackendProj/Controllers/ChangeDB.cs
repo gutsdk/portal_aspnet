ec36e89969854ac7b1f7e49bea5a8be61bfb9ea6
Александр Прокашев <proxelb@gmail.com>
2022-11-13 16:48:52 +0400

    добавлено изменение в базе

---
+++ Solution/BackendProj/Controllers/ChangeDB.cs
@@ -0,0 +1,15 @@
+﻿using BackendProj.Models;
+
+namespace BackendProj.Controllers
+{
+    public class ChangeDB
+    {
+        public static void SetUser(User user)
+        {
+            AppContext db = new AppContext();
+
+            db.Update(user);
+            db.SaveChanges();
+        }
+    }
+}
