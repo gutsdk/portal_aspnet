0ce89fdb74ff023576e1dd30ec606e24e27635e8
Azamat16yo <azatka-02@mail.ru>
2022-12-17 11:04:04 +0400

    Ничего важного

--- Solution/BackendProj/Controllers/Authorization.cs
+++ Solution/BackendProj/Controllers/Authorization.cs
@@ -8,7 +8,7 @@ namespace BackendProj.Controllers
         public static User? GetUser(string login, string password)
         {
             AppDbContext db = new AppDbContext();
-
+            if (login == null || password == null) return null;
             var user = db.Users.AsEnumerable().FirstOrDefault(user => user.login == login && Crypto.AreEqual(password, user.password, user.Salt));
 
             return user;
